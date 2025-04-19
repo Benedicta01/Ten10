c50ad07578fd7d3b4b2b7c7ac4d27b680fdc1240
Benedicta01 <akinrulie.ben@gmail.com>
2025-04-19 12:05:02 +0100

    Add files via upload

---
+++ main.java
@@ -0,0 +1,25 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class main {
+
+public static void main(String[] args) {
+
+for(int i=1000000; i>1; i--){
+
+System.out.println("Thev value of i is : " +i);
+
+}
+
+}
+
+// @Test
+
+// void addition() {
+
+// assertEquals(2, 1 + 1);
+
+// }
+
+}
